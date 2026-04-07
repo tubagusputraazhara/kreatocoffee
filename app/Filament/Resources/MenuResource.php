9ddcc6b55b4441b856deea8ad99f6148be5777d3
nadiensitimadiena <nadienmadiena@gmail.com>
2026-04-07 18:37:31 +0700

    Menambahkan master data Menu

---
+++ app/Filament/Resources/MenuResource.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MenuResource\Pages;
+use App\Models\Menu;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+// Komponen Input Form
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Toggle;
+
+// Komponen Kolom Tabel
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\IconColumn;
+
+class MenuResource extends Resource
+{
+    protected static ?string $model = Menu::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                // Id Menu
+                TextInput::make('id_menu')
+                ->label('ID Menu')
+                ->default(function () {
+                $count = \App\Models\Menu::count(); //Menghitung jumlah data di tabel menu
+                $nextNumber = $count + 1; //Menentukan nomor urut berikutnya
+                return 'M' . str_pad($nextNumber, 3, '0', STR_PAD_LEFT); //Menggabungkan huruf 'M' dengan angka yang diformat 3 digit (001)
+        })
+                ->readOnly() // Membuat kotak input tidak bisa diedit manual oleh user
+                ->required() // Wajib untuk diisi
+                ->unique(ignoreRecord: true), // Unique= tidak boleh sama, dan data sudah otomatis
+                
+                // Nama Menu
+                TextInput::make('nama_menu')
+                    ->label('Nama Menu')
+                    ->required(),
+
+                // Harga
+                TextInput::make('harga')
+                    ->label('Harga')
+                    ->numeric()
+                    ->prefix('Rp')
+                    ->required(),
+
+                // Kategori (Makanan/Minuman)
+                Select::make('kategori')
+                    ->label('Kategori')
+                    ->options([
+                        'Makanan' => 'Makanan',
+                        'Minuman' => 'Minuman',
+                    ])
+                    ->required(),
+
+                // Gambar
+                FileUpload::make('gambar')
+                    ->label('Foto Produk')
+                    ->image()
+                    ->directory('menu-images')
+                    ->required(),
+
+                // Deskripsi
+                Textarea::make('deskripsi')
+                    ->label('Deskripsi Menu')
+                    ->required(),
+
+                // Status Menu
+                Toggle::make('is_admin')
+                    ->label('Tersedia')
+                    ->default(true)
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                // Menampilkan ID Menu
+                //TextColumn::make('id')
+                    //->label('ID Menu')
+                    //->sortable(),
+
+                TextColumn::make('id_menu')
+                ->label('ID Menu')
+                ->sortable(),
+
+                // Menampilkan Nama Menu
+                TextColumn::make('nama_menu')
+                    ->label('Nama Menu')
+                    ->searchable(),
+
+                // Menampilkan Harga
+                TextColumn::make('harga')
+                    ->label('Harga')
+                    ->money('idr'),
+
+                // Menampilkan Kategori
+                TextColumn::make('kategori')
+                    ->label('Kategori'),
+
+                // Menampilkan Gambar
+                ImageColumn::make('gambar')
+                    ->label('Gambar')
+                    ->size(50),
+
+                // Menampilkan Deskripsi (Limit karakter agar rapi)
+                TextColumn::make('deskripsi')
+                    ->label('Deskripsi')
+                    ->limit(50),
+
+                // Menampilkan Status Tersedia (is_admin)
+                IconColumn::make('is_admin')
+                    ->label('Status Tersedia')
+                    ->boolean(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMenus::route('/'),
+            'create' => Pages\CreateMenu::route('/create'),
+            'edit' => Pages\EditMenu::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ database/migrations/2026_04_07_112659_create_menus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->string('id_menu')->primary();
+            $table->string('nama_menu');
+            $table->decimal('harga', 12, 2);
+            $table->enum('kategori', ['Makanan', 'Minuman']);
+            $table->string('gambar'); 
+            $table->text('deskripsi'); 
+            $table->boolean('is_admin')->default(true); // Status Menu (Tersedia = 1, Habis = 0)
+            $table->timestamps();
+});
+       
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menu');
+    }
+};
