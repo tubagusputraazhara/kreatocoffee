e46334e86e6d418136b3756c3d7fde01406b57a7
hannasalsabila84 <hannasalsabila84@gmail.com>
2026-04-07 18:37:29 +0700

    menambahkan master data bahanBaku

---
+++ app/Models/bahanBaku.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class bahanBaku extends Model
+{
+    use HasFactory;
+
+    protected $table = 'bahanBaku';
+    protected $guarded = [];
+
+    // ❗ Tambahan penting
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $last = self::orderBy('id', 'desc')->first();
+
+            if (!$last) {
+                $number = 1;
+            } else {
+                $number = (int) substr($last->id, 1) + 1;
+            }
+
+            $model->id = 'B' . str_pad($number, 3, '0', STR_PAD_LEFT);
+        });
+    }
+}
